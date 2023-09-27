5927450ec5e6492e21fe162369d7c2a185a6854e
gcfsalvador <145944423+gcfsalvador@users.noreply.github.com>
2023-09-27 08:48:29 +0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,28 @@
+#Gian Cyrus F. Salvador
+#dunno if it's correct lol
+
+print("Random one on one thing")
+
+print("Hello World!")
+
+#Set Variables
+x = input("Put your name here: ")
+y = input("School name here: ")
+z = input("What are you? ")
+
+print("I am " + x + ", I am a student in " + y + " and also a " + z + ", Thank you for listening.")
+
+print(f"Hello, {x}! I am Python, a diverse Language. Hope we get along together.")
+
+#Age Calcu
+age = int(input("How old are you? "))
+age_next_year = age + 1
+
+print(f"Means you are turning {age_next_year} next year.")
+
+#math
+
+print("Let's do some math!")
+pi = input("What is the first 3 digits of pi? ")
+print(f"Since value of Pi is {pi} as you say it is.")
+print(f"5 modulo 3 us {5%3.14}")
